57a2a9311a916773b70a93e8d6d0204870171eb0
sagawa <sagawa.fumi@gmail.com>
2021-06-24 13:24:36 +0900

    add typescript-react practice

---
+++ typescript-react-practice/src/App.tsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { useState } from "react";
+import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "ふみ",
+  // hobbies: ["映画", "ゲーム"],
+};
+
+function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+  const onCLickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
+  return (
+    <div className="App">
+      <UserProfile user={user} />
+      <Text color={"red"} fontSize={"18px"}>
+        あああ
+      </Text>
+      <button onClick={onCLickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
